1ec0dbff62835ab66625f3c24453973e30f305ad
guntekhunter <guntek2000@gmail.com>
2024-08-20 16:54:33 +0800

    feat: wallpanel count

--- app/3d/canvas/Object.tsx
+++ app/3d/canvas/Object.tsx
@@ -16,80 +16,109 @@ const state = proxy({
   },
 });
 
-export function Model(props: any) {
-  const { nodes, materials } = useGLTF("/wallpanel and uv.glb");
+export function Model(props:any) {
+  const { nodes, materials } = useGLTF('/yang menar.glb')
+  const handleClick = (e:any) => {
+    console.log(e)
+  }
 
-  const handleClick = (name: string) => {
-    console.log("ommaleka", name);
-  };
+  const originalWidth = 1.502; // Assuming this is the original width of the model in meters
+  const originalLength = 1.501; // Assuming this is the original width of the model in meters
+  const originalHight = 0.014; // Assuming this is the original width of the model in meters
+  const originalWall = 1.045; // Assuming this is the original width of the model in meters
+  const maxScale = 3;
+  const minScale = 0.1; 
+  const minScaleHight = 0.0; 
+  const minScaleWall = 0.1; 
+  const scaleFactor = Math.min(maxScale, Math.max(minScale, (props.data.width / 3 ) * originalWidth));
+  const scaleFactorLength = Math.min(maxScale, Math.max(minScale, (props.data.length / 3 ) * originalLength));
+  const scaleFactorHight = Math.min(maxScale, Math.max(minScaleHight, (props.data.hight / 3 ) * originalHight));
+  const scaleFactorWall = Math.min(maxScale, Math.max(minScaleWall, (props.data.hight / 3 ) * originalWall));
 
-  console.log(props.ini);
+  let roundedNumber = 1.502
+  let roundedNumberLength = 1.501
+  let roundedNumberHight = 0.014
+  let roundedNumberWall = 1
+  if(props.data.width ||props.data.hight || props.data.length ){
+    roundedNumber = parseFloat(scaleFactor.toFixed(3));
+    roundedNumberLength = parseFloat(scaleFactorLength.toFixed(3));
+    roundedNumberHight = parseFloat(scaleFactorHight.toFixed(3));
+    roundedNumberWall = parseFloat(scaleFactorWall.toFixed(3));
+  }else{
+    roundedNumber = 1.502;
+    roundedNumberLength = 1.501;
+    roundedNumberHight = 0.014;
+    roundedNumberWall = 1;
+  }
+
+  const newScaleZ = roundedNumberLength;
+  console.log("ini nilainya", )
+  console.log("ini lebarnya", props.data.count)
   return (
     <group {...props} dispose={null}>
-      {nodes.lantai && (
-        <mesh
-          onClick={() => handleClick("lantai")}
-          castShadow
-          receiveShadow
-          geometry={nodes.lantai.geometry}
-          material={materials["Material.004"]}
-          position={[0, 0.058, 0]}
-          scale={4.705}
-        />
-      )}
-      {nodes.wallpanel_kiri && (
-        <mesh
-          onClick={() => handleClick("wallpanel kiri")}
-          castShadow
-          receiveShadow
-          geometry={nodes.wallpanel_kiri.geometry}
-          material={materials["Material.001"]}
-          position={[3.708, 2.808, -4.709]}
-        />
-      )}
-      {nodes.uv_board && (
-        <mesh
-          onClick={() => handleClick("uv board")}
-          castShadow
-          receiveShadow
-          geometry={nodes.uv_board.geometry}
-          material={materials["Material.003"]}
-          position={[0, 2.777, -4.711]}
-        />
-      )}
-      {props.item && (
-        <mesh
-          onClick={() => handleClick("wallpanel_kanan")}
-          castShadow
-          receiveShadow
-          geometry={nodes.wallpanel_kanan.geometry}
-          material={materials["Material.002"]}
-          position={[-3.662, 2.808, -4.709]}
-          scale={[-1, 1, 1]}
-        />
-      )}
       <mesh
-        onClick={() => handleClick("dinding")}
-        material-color="#f234ff"
+      onClick={() => handleClick("dinding")}
         castShadow
         receiveShadow
         geometry={nodes.dinding.geometry}
         material={materials.Material}
-        scale={[4.709, 0.043, 4.709]}
+        position={[1.117, 1.879, -0.799]}
+        scale={[roundedNumber, roundedNumberHight , 1.502]}
       />
+      <mesh
+      onClick={() => handleClick("lantai")}
+        castShadow
+        receiveShadow
+        geometry={nodes.lantai.geometry}
+        material={materials['Material.004']}
+        position={[1.117 , 1.897, -2.300 + newScaleZ]}
+        scale={[roundedNumber,1.501,roundedNumberLength]}
+      />
+      {/* <mesh
+      onClick={() => handleClick("uv")}
+        castShadow
+        receiveShadow
+        geometry={nodes.uv_board.geometry}
+        material={materials['Material.003']}
+        position={[1.111, 2.929, -2.286]}
+        scale={[0.763, 0.499, 0.319]}
+      /> */}
+      <mesh
+      onClick={() => handleClick("wallpanel kanan")}
+        castShadow
+        receiveShadow
+        geometry={nodes.wallpanel_kanan.geometry}
+        material={materials['Material.005']}
+        position={[1.041 + roundedNumber, 1.875 + roundedNumberWall, -2.295]}
+        scale={[1, roundedNumberWall, 1]}
+      />
+      {Array.from({ length: props.data.count }).map((_, index) => (
+        <mesh
+          key={`wallpanel_kiri_${index}`}
+          onClick={() => handleClick("wallpanel kiri")}
+          castShadow
+          receiveShadow
+          geometry={nodes.wallpanel_kanan001.geometry}
+          material={materials['Material.006']}
+          position={[
+            1.041 - roundedNumber + index * 0.155, 
+            1.875 + roundedNumberWall, 
+            -2.295
+          ]}
+          scale={[-1, roundedNumberWall, 1]}
+        />
+      ))}
     </group>
-  );
+  )
 }
 
-useGLTF.preload("/kotak.glb");
+useGLTF.preload('/yang menar.glb')
 
 export default function Object(props: any) {
   const groups = useRef<Group>(null);
-
-  console.log(props.ini);
   return (
     <group ref={groups}>
-      <Model item={props.ini} />
+      <Model item={props.ini} data={props.data}/>
     </group>
   );
 }
--- app/3d/canvas/Scene.tsx
+++ app/3d/canvas/Scene.tsx
@@ -4,17 +4,50 @@ import { Canvas } from "@react-three/fiber";
 import React, { Suspense, useState } from "react";
 import Object from "./Object";
 import Button from "../../component/template/Button";
+import Input from "../../component/template/Input";
 
 export default function Scene() {
   const [items, setItems] = useState(false);
+  const [theData, setTheData] = useState({
+    width: 0,
+    length:0,
+    hight:0
+  })
+  
   const handleClick = () => {
     console.log("ommaleka");
     setItems(!items);
   };
+  
+  const handleInput = (e:any) => {
+    setTheData({ ...theData, [e.target.name]: parseFloat(e.target.value) });
+  }
+
+  console.log(theData)
+
   return (
     <div className="w-full h-[100vh] relative">
-      <div className="w-full flex justify-center absolute z-20">
-        <Button onClick={handleClick}>ommaleka</Button>
+      <div className="w-[20%] flex justify-start absolute z-10">
+        <div className="w-full h-[100vh] shadow-md bg-white border border-r-[1px] border-gray-200 px-[1rem] py-[1rem]">
+          <div className="grid grid-rows-4 grid-flow-col gap-4">
+            <div>
+              <label>Panjang</label>
+              <input onChange={handleInput} name="width"/>
+            </div>
+            <div>
+              <label>Lebar</label>
+              <input onChange={handleInput} name="length"/>
+            </div>
+            <div>
+              <label>Tinggi</label>
+              <input onChange={handleInput} name="hight"/>
+            </div>
+            <div>
+              <label>Jumlah Wallpanel</label>
+              <input onChange={handleInput} name="count"/>
+            </div>
+          </div>
+        </div>
       </div>
       <Canvas>
         <Environment preset="studio" />
@@ -23,7 +56,7 @@ export default function Scene() {
         <mesh>
           <Center>
             <Suspense>
-              <Object ini={items} />
+              <Object ini={items} data={theData}/>
             </Suspense>
           </Center>
         </mesh>
