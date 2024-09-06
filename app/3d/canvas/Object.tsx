5e676aae7e005fa297cc1aa0060d6d6fbf49ea96
guntekhunter <guntek2000@gmail.com>
2024-09-06 23:42:46 +0800

    fix: room object plafon floor and wallpanel

--- app/3d/canvas/Object.tsx
+++ app/3d/canvas/Object.tsx
@@ -5,17 +5,6 @@ import { useGLTF } from "@react-three/drei";
 import { proxy } from "valtio";
 import * as THREE from 'three';
 
-const state = proxy({
-  current: null,
-  items: {
-    wallpanel_kanan: "#ffffff",
-    wallpanel_kiri: "#ffffff",
-    dinding: "#ffffff",
-    lantai: "#ffffff",
-    uv_board: "#ffffff",
-  },
-});
-
 export function Model(props: any) {
   const { nodes, materials } = useGLTF('/yang menar.glb');
   const groupRef = useRef<Group>(null);
@@ -30,71 +19,67 @@ export function Model(props: any) {
   const hight = parseInt(localStorage.getItem("hight"))
   const length = parseInt(localStorage.getItem("length"))
 
-  const wallpanel = width / 2.95
+  console.log("width", width)
+  console.log("length", length)
 
 
-  const originalWidth = 0.8;
-  const originalLength = 2;
-  const originalHight = 1.522;
-  const originalWall = 2;
-  const maxScale = 10;
-  const minScale = 0.1;
-  const minScaleHight = 0.0;
-  const minScaleWall = 0.1;
 
-  const scaleFactor = Math.min(maxScale, Math.max(minScale, (props.data.width || width / 3) * originalWidth));
-  const scaleFactorLength = Math.min(maxScale, Math.max(minScale, (props.data.length || length / 3) * originalLength));
-  const scaleFactorHight = Math.min(maxScale, Math.max(minScaleHight, (props.data.hight || hight / 3) * originalHight));
-  const scaleFactorWall = Math.min(maxScale, Math.max(minScaleWall, (props.data.hight || hight / 3) * originalWall));
 
-  let roundedNumber = 1;
-  let roundedNumberLength = 1.501;
-  let roundedNumberHight = 1.522;
-  let roundedNumberWall = 1;
+  // const wallpanel = width / 2.95
+  // const originalWidth = 0.8;
+  // const originalLength = 2;
+  // const originalHight = 1.522;
+  // const originalWall = 2;
+  // const maxScale = 10;
+  // const minScale = 0.1;
+  // const minScaleHight = 0.0;
+  // const minScaleWall = 0.1;
 
-  if (width || hight || length) {
-    roundedNumber = parseFloat(scaleFactor.toFixed(3));
-    roundedNumberLength = parseFloat(scaleFactorLength.toFixed(3));
-    roundedNumberHight = parseFloat(scaleFactorHight.toFixed(3));
-    roundedNumberWall = parseFloat(scaleFactorWall.toFixed(3));
-  }
+  // const scaleFactor = Math.min(maxScale, Math.max(minScale, (props.data.width || width / 3) * originalWidth));
+  // const scaleFactorLength = Math.min(maxScale, Math.max(minScale, (props.data.length || length / 3) * originalLength));
+  // const scaleFactorHight = Math.min(maxScale, Math.max(minScaleHight, (props.data.hight || hight / 3) * originalHight));
+  // const scaleFactorWall = Math.min(maxScale, Math.max(minScaleWall, (props.data.hight || hight / 3) * originalWall));
 
-  const newScaleZ = roundedNumberLength;
-  const newScaleY = roundedNumberHight;
+  // let roundedNumber = 1;
+  // let roundedNumberLength = 1.501;
+  // let roundedNumberHight = 1.522;
+  // let roundedNumberWall = 1;
 
-  useEffect(() => {
-    if (groupRef.current) {
-      const box = new Box3().setFromObject(groupRef.current);
-      const center = box.getCenter(new Vector3());
+  // if (width || hight || length) {
+  //   roundedNumber = parseFloat(scaleFactor.toFixed(3));
+  //   roundedNumberLength = parseFloat(scaleFactorLength.toFixed(3));
+  //   roundedNumberHight = parseFloat(scaleFactorHight.toFixed(3));
+  //   roundedNumberWall = parseFloat(scaleFactorWall.toFixed(3));
+  // }
 
-      // Adjust the group's position to center it
-      groupRef.current.position.x -= center.x;
-      groupRef.current.position.y -= center.y;
-      groupRef.current.position.z -= center.z;
-    }
-  }, [groupRef.current]);
+  // const newScaleZ = roundedNumberLength;
+  // const newScaleY = roundedNumberHight;
 
-  console.log(roundedNumber)
+  // useEffect(() => {
+  //   if (groupRef.current) {
+  //     const box = new Box3().setFromObject(groupRef.current);
+  //     const center = box.getCenter(new Vector3());
+
+  //     // Adjust the group's position to center it
+  //     groupRef.current.position.x -= center.x;
+  //     groupRef.current.position.y -= center.y;
+  //     groupRef.current.position.z -= center.z;
+  //   }
+  // }, [groupRef.current]);
+
+  // console.log(roundedNumber)
 
   return (
-    <group {...props} dispose={null} ref={groupRef}>
-      {
-        wallpanelCount && (
-          <>
-            {Array.from({ length: wallpanelCount }).map((_, index) => (<mesh
-              castShadow
-              receiveShadow
-              geometry={nodes.wallpanel_kiri.geometry}
-              position={[-0.1 - roundedNumber + index * 0.155, 0 + newScaleY, -1.484]}
-              scale={[1, roundedNumberHight, 1]}
-              material={materials['Material.005']}
-              rotation={[0, 0, -Math.PI]}
-            />
-            ))}
-          </>
-        )
-      }
-
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.wallpanel_kiri.geometry}
+        material={materials['Material.008']}
+        position={[-width - 1.42, 1.519, 0]}
+        rotation={[0, 0, -Math.PI]}
+        scale={[1, hight, 1]}
+      />
       <mesh
         castShadow
         receiveShadow
@@ -109,9 +94,10 @@ export function Model(props: any) {
         receiveShadow
         geometry={nodes.plafon_flat.geometry}
         material={materials.putih}
-        position={[-0.15, 2.259 + roundedNumberHight, 1.15]} scale={[roundedNumber - 0.9, 2, roundedNumberLength - 0.9]}
+        position={[-0.136, 3.747, 0.008]}
       />
-      <group position={[-0.15, 2.259 + roundedNumberHight, 1.15]} scale={[roundedNumber - 0.9, 2, roundedNumberLength - 0.9]} >
+      <group position={[0, hight, 0 + length]}
+        scale={[width / 1.5, 2, length / 1.5]}>
         <mesh
           castShadow
           receiveShadow
@@ -130,39 +116,123 @@ export function Model(props: any) {
         receiveShadow
         geometry={nodes.lantai.geometry}
         material={materials['Material.006']}
-        position={[-0.161, -0.001, -1.500 + newScaleZ]}
-        rotation={[0, Math.PI / 2, 0]}
-        scale={[roundedNumberLength, 0.01, roundedNumber]}
+        position={[0, 0 - hight, 0 + length]}
+        scale={[width, 0.017, length]}
       />
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.dinding_depan.geometry}
         material={nodes.dinding_depan.material}
-        position={[-0.161, 0 + newScaleY, -1.502]}
+        position={[0, 0, 0]} // Adjusted position
         rotation={[Math.PI / 2, 0, 0]}
-        scale={[roundedNumber, 0.017, roundedNumberHight]}
+        scale={[width, 0.017, hight]} // Adjust scale
       />
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.dinding_kanan.geometry}
         material={nodes.dinding_kanan.material}
-        position={[-0.15 + roundedNumber, 0 + newScaleY, -1.500 + newScaleZ]}
+        position={[0 + length, 0, 0 + width]} // Adjusted position
         rotation={[Math.PI / 2, 0, -Math.PI / 2]}
-        scale={[roundedNumberLength, 0.017, roundedNumberHight]}
+        scale={[length, 0.017, hight]} // Adjust scale
       />
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.wallpanel_kiri001.geometry}
         material={materials['Material.005']}
-        position={[-0.168 + roundedNumber, 1.519, -1.406]}
-        // position={[0 + roundedNumber, 0 + roundedNumberWall, -3]}
+        position={[1.341, 1.519, -1.406]}
         rotation={[Math.PI, -Math.PI / 2, 0]}
         scale={[1, 1.521, 1]}
       />
     </group>
+    // <group {...props} dispose={null} ref={groupRef}>
+    //   {
+    //     wallpanelCount && (
+    //       <>
+    //         {Array.from({ length: wallpanelCount }).map((_, index) => (<mesh
+    //           castShadow
+    //           receiveShadow
+    //           geometry={nodes.wallpanel_kiri.geometry}
+    //           position={[-0.1 - roundedNumber + index * 0.155, 0 + newScaleY, -1.484]}
+    //           scale={[1, roundedNumberHight, 1]}
+    //           material={materials['Material.005']}
+    //           rotation={[0, 0, -Math.PI]}
+    //         />
+    //         ))}
+    //       </>
+    //     )
+    //   }
+
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.uv.geometry}
+    //     material={materials['Material.010']}
+    //     position={[0.551, 1.499, -1.502]}
+    //     rotation={[0, 0, -Math.PI / 2]}
+    //     scale={[0.763, 0.499, 0.319]}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.plafon_flat.geometry}
+    //     material={materials.putih}
+    //     position={[-0.15, 2.259 + roundedNumberHight, 1.15]} scale={[roundedNumber - 0.9, 2, roundedNumberLength - 0.9]}
+    //   />
+    //   <group position={[-0.15, 2.259 + roundedNumberHight, 1.15]} scale={[roundedNumber - 0.9, 2, roundedNumberLength - 0.9]} >
+    //     <mesh
+    //       castShadow
+    //       receiveShadow
+    //       geometry={nodes.Cube003.geometry}
+    //       material={materials['Material.014']}
+    //     />
+    //     <mesh
+    //       castShadow
+    //       receiveShadow
+    //       geometry={nodes.Cube003_1.geometry}
+    //       material={materials['Material.001']}
+    //     />
+    //   </group>
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.lantai.geometry}
+    //     material={materials['Material.006']}
+    //     position={[-0.161, -0.001, -1.500 + newScaleZ]}
+    //     rotation={[0, Math.PI / 2, 0]}
+    //     scale={[roundedNumberLength, 0.01, roundedNumber]}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.dinding_depan.geometry}
+    //     material={nodes.dinding_depan.material}
+    //     position={[-0.161, 0 + newScaleY, -1.502]}
+    //     rotation={[Math.PI / 2, 0, 0]}
+    //     scale={[roundedNumber, 0.017, roundedNumberHight]}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.dinding_kanan.geometry}
+    //     material={nodes.dinding_kanan.material}
+    //     position={[-0.15 + roundedNumber, 0 + newScaleY, -1.500 + newScaleZ]}
+    //     rotation={[Math.PI / 2, 0, -Math.PI / 2]}
+    //     scale={[roundedNumberLength, 0.017, roundedNumberHight]}
+    //   />
+    //   <mesh
+    //     castShadow
+    //     receiveShadow
+    //     geometry={nodes.wallpanel_kiri001.geometry}
+    //     material={materials['Material.005']}
+    //     position={[-0.168 + roundedNumber, 1.519, -1.406]}
+    //     // position={[0 + roundedNumber, 0 + roundedNumberWall, -3]}
+    //     rotation={[Math.PI, -Math.PI / 2, 0]}
+    //     scale={[1, 1.521, 1]}
+    //   />
+    // </group>
     // <group ref={groupRef} {...props} dispose={null}>
     //   <mesh
     //     onClick={() => handleClick("dinding")}
